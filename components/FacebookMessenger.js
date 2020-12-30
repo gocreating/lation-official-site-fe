7ae00465940f4a53918944d536e1b45849db229e
gocreating <gocreating@gmail.com>
2020-12-30 19:03:27 +0800

    Update

--- components/FacebookMessenger.js
+++ components/FacebookMessenger.js
@@ -5,28 +5,26 @@ export const FacebookMessengerHeader = () => {
   return (
     <div suppressHydrationWarning={true}>
       <div id="fb-root" />
-      {process.browser && (
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml            : true,
-                  version          : 'v9.0'
-                });
-              };
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v9.0'
+              });
+            };
 
-              (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));
-            `
-          }}
-        />
-      )}
+            (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          `
+        }}
+      />
     </div>
   )
 }
