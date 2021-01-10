317a191d660ea31c211dc6e9f0611329af8bf395
翁治平 <gocreating@gmail.com>
2021-01-10 07:00:33 +0000

    Remove unused import

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import AppLayout from '../components/AppLayout'
-import Products from '../components/Products'
 import { withTranslation } from '../i18n'
 
 const NotFoundPage = ({ t }) => {
